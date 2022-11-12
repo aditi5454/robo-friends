90476e4997a69131b5cffc89f2d21a303b0e2d22
Aditi <aditigarg2028@gmail.com>
2022-11-13 03:28:24 +0530

    adding scroll

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
 import React, { Component } from "react";
 import CardList from './CardList.jsx';
-import { robots } from './robots.js';
+// import { robots } from './robots.js';
 import SearchBar from './SearchBar.jsx';
 import "./app.css"
+import Scroll from './Scroll.js'
 
 
 class App extends Component {
     constructor() {
         super();
         this.state = {
-            robots: robots,
+            // robots: robots,
+            robots: [],
             searchfield: '',
         }
+    }
+
+    componentDidMount(){
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response=> response.json())
+        .then(users=>this.setState({robots:users}))
     }
 
     onSearchChange = (event) => {
@@ -19,14 +27,17 @@ class App extends Component {
     }
 
     render() {
-        const filteredRobots = this.state.robots.filter(robot => {
-            return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
+        const {robots, searchfield} = this.state;
+        const filteredRobots = robots.filter(robot => {
+            return robot.name.toLowerCase().includes(searchfield.toLowerCase())
         });
         return (
             <div className="app">
                 <h1>RoboFriends</h1>
                 <SearchBar searchChange={this.onSearchChange} />
+                <Scroll>
                 <CardList robots={filteredRobots} />
+                </Scroll>
             </div>
         )
     }
